724f17dfa0ab4153ad99cda126ef5f419f1c1212
Steph Crown <emmanuelstephen024@gmail.com>
2025-12-22 12:49:17 +0100

    Implement separaion of concerns

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+/// CLI tool to search for files within a directory (Simple version of `find`)
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CliArgs {
+  /// Pattern to search for
+  pattern: String,
+
+  /// Directory to search within
+  directory: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+  if let Err(error) = run() {
+    eprintln!("filesearch: {}", error);
+    std::process::exit(1)
+  }
+}
+
+fn run() -> Result<(), String> {
+  let args = parse_input();
+  let _b = validate_args(&args)?;
+
+  Ok(())
+}
+
+fn parse_input() -> CliArgs {
+  CliArgs::parse()
+}
+
+fn validate_args(args: &CliArgs) -> Result<&CliArgs, String> {
+  if args.pattern == "aaa" {
+    Ok(args)
+  } else {
+    Err(format!("{}: invalid argument", args.pattern))
+  }
 }
